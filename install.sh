6e0bd0dd98332c97633fa84716388b120a4ee20e
Ryan <ryant18@vt.edu>
2020-05-27 15:36:52 -0400

    install scrip update

--- install.sh
+++ install.sh
@@ -6,13 +6,16 @@ cp -r ./background ~/.config/
 cp -r ./i3 ~/.config/
 cp -r ./i3status ~/.config/
 cp -r ./i3blocks ~/.config/
-cp -r ./nvim ~/.config/
 
-cp .vimrc ~
-cp -r ./.vim ~
+#cp -r ./nvim ~/.config/
+#cp .vimrc ~
+#cp -r ./.vim ~
 
 cp .zshrc ~
 cp -r ./.zsh ~
+#cp .bashrc ~
+#cp .inputrc ~
+#cp .bash_aliases ~
 
 cp -r .scripts/ ~
 
